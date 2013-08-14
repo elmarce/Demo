c729fe7015b14a30a43c50f0005b0866f4214407
elmarce <elmarce@hotmail.de>
2013-08-14 08:38:31 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bounda::Application.config.secret_key_base = 'a27c6c466e9c7f84d32e3aaf628985cb9cf2a28a93fb708e5a511f2be3ed3df82c8edf84eb1e75ea2f17295c6928b903a16555311ec4b81367c632d13f10de6d'
